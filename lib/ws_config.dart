cef7a71e349e62533b9dda5c2191c678662a78ed
Kiattikul <mju6304106304@mju.ac.th>
2023-08-21 00:24:01 +0700

    test

--- lib/ws_config.dart
+++ lib/ws_config.dart
@@ -2,13 +2,16 @@
 
 //ip หอ
 const String ipv4 = '192.168.88.71';
+//
 
 //ip ม.
 //const String ipv4 = '10.11.24.187';
 
-const Map<String, String> headers = {"Access-Control-Allow-Origin": "*",
-      'Content-Type': 'application/json',
-      'Accept-Language': 'th',
-      'Accept': '*/*'};
+const Map<String, String> headers = {
+  "Access-Control-Allow-Origin": "*",
+  'Content-Type': 'application/json',
+  'Accept-Language': 'th',
+  'Accept': '*/*'
+};
 
 const String baseURL = "http://" + ipv4 + ":8080";
