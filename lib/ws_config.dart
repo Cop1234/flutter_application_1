2abd836d0114a7106e72d6ad9bec40aed0a3ae6f
copter6318 <MJU6304106318@mju.ac.th>
2024-02-11 09:26:15 +0700

    change config

--- lib/ws_config.dart
+++ lib/ws_config.dart
@@ -3,12 +3,12 @@
 //ip หอ
 //C
 //const String ipv4 = '192.168.172.202';
-const String ipv4 = '192.168.88.81';
+//const String ipv4 = '192.168.88.81';
 //P
 //const String ipv4 = '192.168.199.130';
 
 //ip ม.
-//const String ipv4 = '10.11.10.126';
+const String ipv4 = '10.100.35.12';
 //const String ipv4 = '172.16.2.252';
 
 const Map<String, String> headers = {
